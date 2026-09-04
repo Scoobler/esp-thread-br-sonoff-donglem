5766f75638d1ed8f81038da76f85b4402e97d44f
Scoobler <jamienwood@hotmail.com>
2026-09-04 16:58:24 +0100

    fix(board): preserve RCP updater baud

--- examples/basic_thread_border_router/main/esp_ot_config.h
+++ examples/basic_thread_border_router/main/esp_ot_config.h
@@ -80,7 +80,7 @@
 #define ESP_OPENTHREAD_RCP_UPDATE_CONFIG()                                                                   \
     {                                                                                                        \
         .rcp_type = RCP_TYPE_UART, .uart_rx_pin = CONFIG_PIN_TO_RCP_TX, .uart_tx_pin = CONFIG_PIN_TO_RCP_RX, \
-        .uart_port = CONFIG_ESP_BR_RCP_UART_PORT, .uart_baudrate = CONFIG_ESP_BR_RCP_UART_BAUDRATE,          \
+        .uart_port = CONFIG_ESP_BR_RCP_UART_PORT, .uart_baudrate = 115200,                                   \
         .reset_pin = CONFIG_PIN_TO_RCP_RESET,                                                                \
         .boot_pin = CONFIG_PIN_TO_RCP_BOOT, .update_baudrate = 460800,                                       \
         .firmware_dir = "/" CONFIG_RCP_PARTITION_NAME "/ot_rcp", .target_chip = ESP_BR_RCP_TARGET_ID         \
